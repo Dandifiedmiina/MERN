9ba9ada615768c8d33ace397821bf55c1551f0ee
Dandifiedmiina <63451212+Dandifiedmiina@users.noreply.github.com>
2021-03-04 10:02:24 +0200

    project1

--- WS4/ws4.js
+++ WS4/ws4.js
@@ -1,17 +1,26 @@
-// Otetaan axios-moduuli käyttöön
+
 var axios = require("axios");
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+if (request.url === "/movies") {
 // Luodaan AJAX-kysely ja lähetetään pyyntö
 const promise = axios
- .get("http://www.omdbapi.com/?s=star+wars&apikey=cbbc6750")
- // Käsitellään vastaus kun se saapuu
-  .then(response => {
+  .get("http://www.omdbapi.com/?s=star+wars&apikey=cbbc6750")
+  // Käsitellään vastaus kun se saapuu
+  .then((response) => {
     const data = response.data;
+    for (var i = 0; i < movies.Search.length; i++) {
+      response.write
+        (movies.Search[i].Title);
+    }
+
     console.log(data);
   });
-// Tulostetaan konsoliin promise-olion tilatietoja AJAX-pyynnön käsittelyn aikana  
+    };
+// Tulostetaan konsoliin promise-olion tilatietoja AJAX-pyynnön käsittelyn aikana
 console.log(promise);
-
-// Tehdään silmukka joka käsittelee tulosjoukkona saadun tauluko
-for (var i = 0; i < movies.Search.length; i++) {
-        console.log(movies.Search[i].Title);
-    }
+  });
+  .listen(PORT);
