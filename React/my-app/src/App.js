8d1ee27ad0ecd126d3c2f159c20e1d49a3e663b6
Dandifiedmiina <63451212+Dandifiedmiina@users.noreply.github.com>
2021-04-30 22:12:19 +0300

    mern

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,6 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table} from "react-bootstrap";
+
+const CompanyArray = (props) => {
+  const { data } = props;
+
+  return (
+    <div className="resultstabl2">
+      <SearchBar />
+      <Table striped responsive="sm" hover>
+        <thead class="thead-dark">
+          <tr key={props.id}>
+            <th scope="col"></th>
+            <th scope="col">Name</th>
+            <th scope="col">Homepage</th>
+            <th scope="col">Founded</th>
+            <th scope="col">Updated:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, i) => (
+            <tr>
+              <th scope="row"></th>
+              <td key={i}> {item.name}</td>
+              <td> {item.homepage_url} </td>
+              <td> {item.founded_day}/{item.founded_month}/{item.founded_year}  </td>
+              <td> email: {item.email_address}, phone: {item.phone_number} </td>
+              {}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+
+const SearchBar = () => {
+
+
+  const [query, setQuery] = useState('')
+  // Määritellään käsittelija napille 1
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Tapahtuman aiheutti: ", event.target);
+    console.log("Hakusana: ", query);
+    GetCompanyInfo();
+  };
+
+  // Määritellään käsittelija napille 2
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log("Tapahtuman aiheutti: ", event.target);
+    GetData();
+  };
+  
+  const [results, setResults] = useState([])
 
 const GetData = () => {
   fetch("https://restapimiina.herokuapp.com/api/getall")
@@ -16,62 +73,43 @@ const GetData = () => {
       );
     });
   return <div>Nothing here. Fething data...</div>;
+  }; 
+
+
+const GetCompanyInfo = () => {
+  fetch("https://restapimiina.herokuapp.com/name/" + query )
+    .then((results) => {
+      return results.json();
+    })
+    .then((data) => {
+      console.log("Haettu", data);
+      const items = data;
+      console.log("Company:", data);
+
+      setResults(items)
+    });
 };
-
-const CompanyArray = (props) => {
-  const { data } = props;
-
-  return (
-    <div>
-      <SearchBar />
-      <table className="table table-striped table-bordered">
-        <thead>
-          <tr key={props.id}>
-            <th scope="col">Name</th>
-            <th scope="col">Homepage</th>
-            <th scope="col">overview</th>
-            <th scope="col">Temperature</th>
-            <th scope="col">Updated:</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, i) => (
-            <tr>
-              <td key={i}> {item.name}</td>
-              <td> {item.homepage_url} </td>
-              <td> {item.overview} </td>
-              <td> {item.updated_at}</td>
-              {}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-
-const SearchBar = () => {
-  // Määritellään käsittelija napille 1
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Tapahtuman aiheutti: ", event.target);
-    var form = event.target;
-    console.log("Hakusana: ", form.query.value);
-  };
-
-  // Määritellään käsittelija napille 2
-  const handleClick = (event) => {
-    event.preventDefault();
-    console.log("Tapahtuman aiheutti: ", event.target);
-    GetData();
-  };
+  
   // Komponentin palauttama JSX muotoinen esitys
-  return (
-    <div>
-      <h1>Search for a company</h1>
-      <div>
+   return (
         <form onSubmit={handleSubmit}>
+         
+          <div className="form-group">
+            <h1>Search for a company</h1>
+            <label>Hae: </label>
+            <input
+              type="search"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              className="form-control"
+              placeholder="ex. Technorati"
+              name="query"
+            />
+          </div>
+          <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
           <div className="form-group">
             <button
               type="button"
@@ -80,11 +118,11 @@ const SearchBar = () => {
             >
               Hae kaikki
             </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
-
+            </div>
+              <CompanyArray data={results} />
+            </div>
+          
+          </form>
+    );
+  };
 export default SearchBar;
